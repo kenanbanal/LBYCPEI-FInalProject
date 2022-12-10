c91be4c22c6d8a76890b0219c3279d9599ea0cba
kenanbanal <77086714+kenanbanal@users.noreply.github.com>
2022-12-10 18:24:31 +0800

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.opencsvdemo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.opencsv;
-    requires org.controlsfx.controls;
-
-    opens com.example.opencsvdemo to javafx.fxml;
-    exports com.example.opencsvdemo;
-}
